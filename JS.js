8a82e67807fc570413ebdb7af5b7be975807588a
Radosław Maksym Hołubowski <holubowskir@gmail.com>
2026-03-16 16:37:30 +0100

    Create JS.js

---
+++ JS.js
@@ -0,0 +1,12 @@
+function constants() {
+    const inputOne = document.getElementById("NO-ONE").value;
+    const inputTwo = document.getElementById("NO-TWO").value;
+    const output = document.getElementById("OUTPUT");
+    const button = document.getElementById("BUTTON-C");
+}
+
+function calculate() {
+    console.log("Hello World");
+}
+
+input();
